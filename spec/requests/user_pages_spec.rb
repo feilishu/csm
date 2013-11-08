31e5de81be57c344e68e007ff98b73fba097e2bb
FeiLishu <feilishu@gmail.com>
2013-11-08 16:37:01 +0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,6 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    before { visit signup_path }
-
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
@@ -27,6 +25,20 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
